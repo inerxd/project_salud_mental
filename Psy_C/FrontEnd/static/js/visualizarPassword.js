63fa93539dc2d04a65bfc474a696af7611369080
javier <inerxd@gmail.com>
2024-09-18 00:56:31 -0600

    diseño de login

--- Psy_C/FrontEnd/static/js/visualizarPassword.js
+++ Psy_C/FrontEnd/static/js/visualizarPassword.js
@@ -1,11 +1,15 @@
-function mostrarOcultarContrasena() {
+function mostrarOcultarContrasena(e) {
+    e.preventDefault();
+   
     var contrasenaInput = document.getElementById("password");
     var iconoMostrarOcultar = document.getElementById("iconoMostrarOcultar");
 
     if (contrasenaInput.type === "password") {
+      
         contrasenaInput.type = "text";
         iconoMostrarOcultar.innerHTML = '<i class="fas fa-eye-slash"></i>';
     } else {
+   
         contrasenaInput.type = "password";
         iconoMostrarOcultar.innerHTML = '<i class="fas fa-eye"></i>';
     }
